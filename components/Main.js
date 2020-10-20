91bd2ec4176feb01b1cbb1d6614598ae0639b281
Prisca Roseline <prisca.ros@onja.org>
2020-10-20 16:29:40 +0300

    I layed out cards.

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import places from '../stays.json';
 
-let data = require('../stays.json')
-console.log(data);
+let data = require('../stays.json');
 
 function Main() {
   return (
     <section>
       <h2>Stays in Finland</h2>
       <div className="container">
-        {data.forEach(data => {
-          return data.id = Date.now();
-        })}
         {data.map(data => {
           return (
             <div key={data.title} className="cards">
@@ -23,7 +19,10 @@ function Main() {
                   <p className="desc">.{data.beds} beds</p>
                 </div>
                 <div className="wrapper">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill="#EB5757" /></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px">
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" fill="#EB5757" />
+                  </svg>
                   <span className="rating">{data.rating}</span>
                 </div>
               </div>
---
+++ components/Modal.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// let data = require('../stays.json');
+// let data = require('../stays.json');
+
+// function UseModal() {
+//   return (
+//     <div className="outter-modal">
+//       {data.map(data => {
+//         return (
+//           <div className="inner-modal" key={data.title}>
+//             <p>{data.city}</p>
+//           </div>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+
+// export default UseModal;
